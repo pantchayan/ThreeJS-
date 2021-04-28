6b17a5db5c78e1e3cb261f8c2675badfef64eb63
pantchayan <pantchayan@gmail.com>
2021-04-29 01:55:34 +0530

    Toggling canvas to fullscreen

--- Part1/Webpack/src/script.js
+++ Part1/Webpack/src/script.js
@@ -1,7 +1,7 @@
-import './style.css'
-import * as THREE from 'three'
-import gsap from 'gsap'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 console.log(gsap);
 // Scene
@@ -11,71 +11,92 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
 const cube1 = new THREE.Mesh(geometry, material);
-cube1.position.x=-2
+cube1.position.x = 0;
 
-const geometry2 = new THREE.BoxGeometry(1, 1, 1);
-const material2 = new THREE.MeshBasicMaterial({ color: "#00ff00" });
-const cube2 = new THREE.Mesh(geometry2, material2);
-cube2.position.x = 2;
+scene.add(cube1);
 
+// Green cube {object}
+// const geometry2 = new THREE.BoxGeometry(1, 1, 1);
+// const material2 = new THREE.MeshBasicMaterial({ color: "#00ff00" });
+// const cube2 = new THREE.Mesh(geometry2, material2);
+// cube2.position.x = 2;
 
-const group = new THREE.Group();
-group.add(cube1);
-group.add(cube2);
-
+// const group = new THREE.Group();
+// group.add(cube1);
+// group.add(cube2);
+// scene.add(group)
 
 const axesHelper = new THREE.AxesHelper();
-scene.add( axesHelper );
-
-scene.add(group)
+scene.add(axesHelper);
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
+window.addEventListener("resize", () => {
+  // updating sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // render update
+  renderer.setSize(sizes.width, sizes.height);
+
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// double click to fullscreen
+window.addEventListener("dblclick", () => {
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 // camera.position.x = 2
 // camera.position.y = 2
 camera.position.z = 3;
-camera.lookAt(cube2.position);
+camera.lookAt(cube1.position);
 scene.add(camera);
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // ...
 
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-controls.keys = {
-	LEFT: 'ArrowLeft', //left arrow
-	UP: 'ArrowUp', // up arrow
-	RIGHT: 'ArrowRight', // right arrow
-	BOTTOM: 'ArrowDown' // down arrow
-}
+const controls = new OrbitControls(camera, canvas);
+// controls.enabled =  false;
+controls.enableDamping = true;
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// const clock = new THREE.Clock()
 
+const tick = () => {
+  // const elapsedTime = clock.getElapsedTime()
 
-const clock = new THREE.Clock()
+  // Update objects
+  // cube1.rotation.y = elapsedTime* Math.PI *0.5;
+  controls.update();
+  // Render
+  renderer.render(scene, camera);
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-const tick = () =>
-{
-  const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    // cube1.rotation.y = elapsedTime* Math.PI *0.5;
-    controls.update();
-    // Render
-    renderer.render(scene, camera)
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+tick();
